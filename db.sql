17666c848edf355f6575ea927a949f99bf35b3ae
wednesday.developer <wednesday.developer@gmail.com>
2025-05-05 11:27:09 +0900

    feat: Update todos table policies for authenticated access and remove sample data

--- db.sql
+++ db.sql
@@ -1,29 +1,25 @@
 -- Create the table
 create table todos (
   id bigint primary key generated always as identity,
+  user_id uuid not null references auth.users(id) default auth.uid(),
   content text not null,
   completed boolean default false
 );
--- Insert some sample data into the table
-insert into todos (content)
-values
-  ('牛乳を買う'),
-  ('チェロを弾く'),
-  ('洗車する');
+
 alter table todos enable row level security;
 
 create policy "public can read todos"
-on public.todos
-for select to anon
-using (true);
+on "public"."todos"
+for select to authenticated
+using ( (select auth.uid()) = user_id );
 
 create policy "public can insert todos"
 on "public"."todos"
-for insert to anon
-with check (true);
+for insert to authenticated
+with check ( (select auth.uid()) = user_id );
 
 create policy "public can update todos"
 on "public"."todos"
-for update to anon
-using (true)
-with check (true);
+for update to authenticated
+using ( (select auth.uid()) = user_id )
+with check ( (select auth.uid()) = user_id );
