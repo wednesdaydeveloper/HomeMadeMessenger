2c4cd4a5328ce09cf208b54b57856e3fd755d9e1
wednesday.developer <wednesday.developer@gmail.com>
2025-05-06 15:14:18 +0900

    fix: Improve formatting of authUrl construction for OAuth sign-in

--- src/app/login/actions.ts
+++ src/app/login/actions.ts
@@ -31,9 +31,12 @@ const signup = async (formData: LoginForm): Promise<SubmitHandler<LoginForm>> =>
 
 const signInWith = (provider: Provider) => async () => {
   const supabase = await createClient()
-  const authUrl = (process.env.VERCEL_URL
+  const authUrl = (
+    process.env.VERCEL_URL
     ? `https://${process.env.VERCEL_URL}`
-    : 'http://localhost:3000') + '/auth/callback?next=/todolist'
+    : 'http://localhost:3000'
+    )
+    + '/auth/callback?next=/todolist'
   console.log('authUrl: ', authUrl)
   const { data, error } = await supabase.auth.signInWithOAuth({
     provider: provider,
